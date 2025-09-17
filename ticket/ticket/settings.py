b649e9d8fb5a8fcd6235385b88ddc73000cc38ce
jpcastro14 <castrovoip@gmail.com>
2025-09-17 19:30:56 -0300

    Feat: Add cors headers

--- ticket/ticket/settings.py
+++ ticket/ticket/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
